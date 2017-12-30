e4c3da04b5a3332e5a5ecfb3362aaa84c0a18161
Kelly Geng <gengx@MiamiOH.edu>
2017-12-29 23:34:04 -0500

    feat:implement the game logic

--- js/app.js
+++ js/app.js
@@ -10,6 +10,27 @@
  *   - add each card's HTML to the page
  */
 
+let cards = document.querySelectorAll('.card');
+const totalCards = cards.length;
+let totalMoves = 0;
+
+newOrder = shuffle(Object.keys(cards));
+
+const deckElement = document.createElement('ul');
+deckElement.className = 'deck';
+
+for(var i=0; i < newOrder.length; i++) {
+	deckElement.appendChild(cards[newOrder[i]]);
+	cards[i].addEventListener('click', revealAndProcess);
+};
+
+document.querySelector('.container').replaceChild(deckElement, document.querySelector('.deck'));
+
+document.querySelector('.restart').addEventListener('click', function(){
+	location.reload();
+});
+
+
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -21,9 +42,102 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
+
+
+function revealAndProcess(event) {
+	let openCard = document.querySelector('.open');
+
+	if(event.target.nodeName === 'LI') {
+		showCard(event.target);
+		if(openCard != null ) {
+			if(event.target.firstElementChild.className
+				!== openCard.firstElementChild.className) {
+				setTimeout(hideUnmatchingCards, 500, event.target, openCard);
+				increaseMoves();
+			} else {
+				setTimeout(showMatchingCards, 500, event.target, openCard);
+				increaseMoves();
+			}
+		}
+	}
+}
+
+function showCard(card) {
+	card.classList.add('open');
+	card.classList.add('show');
+	card.removeEventListener('click', revealAndProcess)
+
+}
+
+function hideUnmatchingCards(card1, card2) {
+	card1.classList.remove('open');
+	card1.classList.remove('show');
+
+	card2.classList.remove('open');
+	card2.classList.remove('show');
+
+	card1.addEventListener('click', revealAndProcess)
+	card2.addEventListener('click', revealAndProcess)
+}
+
+function showMatchingCards(card1, card2) {
+	card1.classList.remove('open');
+	card1.classList.remove('show');
+	card1.classList.add('match');
+
+	card2.classList.remove('open');
+	card2.classList.remove('show');
+	card2.classList.add('match');
+
+	card1.removeEventListener('click', revealAndProcess);
+	card2.removeEventListener('click', revealAndProcess);
+
+	if(document.querySelectorAll('.match').length === totalCards) {
+		showResultPage();
+	}
+}
+
+function increaseMoves() {
+	totalMoves++;
+	document.querySelector('.moves').textContent = totalMoves;
+}
+
+function showResultPage() {
+	const fragment = document.createElement('div');
+	fragment.className = 'container';
+
+	const newImgElement = document.createElement('img');
+	newImgElement.src = 'img/checkmark.gif';
+	newImgElement.className = 'checkmark-img';
+	newImgElement.alt = 'Animated checkmark image';
+	fragment.appendChild(newImgElement);
+
+
+	const newHeadingElement = document.createElement('h1');
+	newHeadingElement.innerText = 'Congratulations! You won!';
+	fragment.appendChild(newHeadingElement);
+
+	const newTextElement1 = document.createElement('p');
+	newTextElement1.innerText = 'With ' + totalMoves + ' moves and 3 stars.';
+	fragment.appendChild(newTextElement1);
+
+	const newTextElement2 = document.createElement('p');
+	newTextElement2.innerText = 'Wooooo!';
+	fragment.appendChild(newTextElement2);
+
+	const buttonElement = document.createElement('button');
+	buttonElement.innerText = 'Play Again!';
+	buttonElement.addEventListener('click', function() {
+		location.reload();
+	});
+	fragment.appendChild(buttonElement);
+
+	document.body.replaceChild(fragment, document.querySelector('.container'));
+}
+
+
 
 
 /*
